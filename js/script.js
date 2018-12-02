d711e3012d763b017f1ee484dd6edcce3a31c30b
James Mwangi <james_mwangi@aol.com>
2018-12-02 14:52:00 +0300

    add floats and image and change background color

--- js/script.js
+++ js/script.js
@@ -1,23 +1,27 @@
 var tTrack = function() {
-    var sideA = document.getElementById("one").value;
-    var sideB = document.getElementById("two").value;
-    var sideC = document.getElementById("three").value;
+    var sideA = parseInt(document.getElementById("one").value);
+    var sideB = parseInt(document.getElementById("two").value);
+    var sideC = parseInt(document.getElementById("three").value);
     var ans = document.getElementById("answer");
-    if (sideA === sideB && sideB === sideC) {
-     ans.innerHTML = 'Your shape is an Equilateral triangle!';
+    if (sideB+sideC < sideA || sideC+sideA < sideB || sideB+sideA < sideC) {
+        ans.innerHTML = 'Your shape is NOT a triangle!';
    }
-   else if (sideA === sideB && sideB !== sideC && sideA !== sideC && sideA + sideB > sideC) {
+   else if (sideA===sideB && sideB===sideC) {
+       ans.innerHTML = 'Your shape is an Equilateral triangle!';
+   }
+   else if (sideA+sideB > sideC && sideB+sideC > sideA && sideA+sideC > sideB && sideA === sideB && sideB !== sideC && sideA !== sideC) {
        ans.innerHTML = 'Your shape is an Isosceles triangle!';
    }
-   else if (sideB !== sideA && sideC !== sideA && sideB === sideC && sideB + sideC > sideA) {
+   else if (sideA+sideB > sideC && sideB+sideC > sideA && sideA+sideC > sideB && sideB === sideC && sideB !== sideA && sideC !== sideA) {
        ans.innerHTML = 'Your shape is an Isosceles triangle!';
    }
-   else if (sideB !== sideC && sideB !== sideA && sideA === sideC && sideA + sideC > sideB) {
+   else if (ssideA+sideB > sideC && sideB+sideC > sideA && sideA+sideC > sideB && sideA === sideC && sideC !== sideB && sideA !== sideB) {
            ans.innerHTML = 'Your shape is an Isosceles triangle!';
    }
-   else if (sideA > sideB + sideC && sideB > sideC + sideA && sideC > sideA + sideB) {
-       ans.innerHTML = 'Your shape is NOT a triangle!';
-   } else {
+   else if (sideA+sideB > sideC && sideB+sideC > sideA && sideA+sideC > sideB && sideA !== sideB && sideB !== sideC && sideC !== sideA){
+       ans.innerHTML = 'Your shape is an Scalene triangle!';
+   }
+   else {
        ans.innerHTML = 'Your shape is an Scalene triangle!';
    }
 };
